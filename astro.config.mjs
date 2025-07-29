6a4ecdededf9f0fb1df3e8799b74ec6145b6fb32
Michele Vannucci <michele.vannucci20@gmail.com>
2025-07-29 21:18:45 +0200

    added about

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({  // can add the server option here if needed to render on server side
   adapter: cloudflare()
 });
