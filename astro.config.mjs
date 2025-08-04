075a0665b4a3c9565d872f532f03a52823fdd15b
Michele Vannucci <michele.vannucci20@gmail.com>
2025-08-04 18:13:21 +0200

    added rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({  // can add the server option here if needed to render on server side
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  site: 'https://website-c1d.pages.dev/', // replace with your actual site URL
 });
