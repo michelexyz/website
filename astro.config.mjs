655cb73e6842f3231eb1f09e4053b75a8dc0ec0f
Michele Vannucci <michele.vannucci20@gmail.com>
2025-08-10 21:03:01 +0200

    using content collection for posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
-export default defineConfig({  // can add the server option here if needed to render on server side
+export default defineConfig({
+  // can add the server option here if needed to render on server side
   adapter: cloudflare(),
-  site: 'https://website-c1d.pages.dev/', // replace with your actual site URL
+
+  // replace with your actual site URL
+  site: 'https://website-c1d.pages.dev/',
+
+  integrations: [preact()]
 });
